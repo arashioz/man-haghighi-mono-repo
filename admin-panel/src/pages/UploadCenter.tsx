4255727f01c36353022f5cf7198696449a0ff8ff
Arash Balali <a.balali@ittalie.com>
2025-12-08 19:16:59 +0330

    changes

--- admin-panel/src/pages/UploadCenter.tsx
+++ admin-panel/src/pages/UploadCenter.tsx
@@ -313,6 +313,11 @@ const UploadCenter: React.FC = () => {
         </div>
       ) : (
         <EmptyState
+          icon={
+            <svg className="w-12 h-12 text-gray-400" fill="currentColor" viewBox="0 0 24 24">
+              <path d="M14 2H6c-1.1 0-1.99.9-1.99 2L4 20c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm2 16H8v-2h8v2zm0-4H8v-2h8v2zm-3-5V3.5L18.5 9H13z" />
+            </svg>
+          }
           title="فایلی یافت نشد"
           description="هیچ فایلی در این دسته‌بندی وجود ندارد"
         />
