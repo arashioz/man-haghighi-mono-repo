45a8b93cd0ab3985c05438251ec63a00b31692f3
Arash Balali <a.balali@ittalie.com>
2026-02-11 14:26:38 +0330

    fix upload

--- backend/src/upload-center/upload-center.module.ts
+++ backend/src/upload-center/upload-center.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UploadCenterController } from './upload-center.controller';
 import { UploadCenterService } from './upload-center.service';
 import { PrismaModule } from '../common/prisma/prisma.module';
+import { CloudStorageModule } from '@/cloud-storage/cloud-storage.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule,CloudStorageModule],
   controllers: [UploadCenterController],
   providers: [UploadCenterService],
   exports: [UploadCenterService],
--- backend/src/uploads/uploads.module.ts
+++ backend/src/uploads/uploads.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UploadsService } from './uploads.service';
 import { UploadsController } from './uploads.controller';
+import { CloudStorageModule } from '../cloud-storage/cloud-storage.module';
 
 @Module({
-  controllers: [UploadsController],
+  imports:[CloudStorageModule]
+,  controllers: [UploadsController],
   providers: [UploadsService],
   exports: [UploadsService],
 })
