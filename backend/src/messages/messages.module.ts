d773be32bceeefe2697774afebe36442b9004ae4
Arash Balali <a.balali@ittalie.com>
2026-02-12 16:46:09 +0330

    changes

--- backend/src/messages/messages.module.ts
+++ backend/src/messages/messages.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MessagesService } from './messages.service';
+import { MessagesController } from './messages.controller';
 import { SmsModule } from '../sms/sms.module';
 import { PrismaModule } from '../common/prisma/prisma.module';
 
 @Module({
   imports: [SmsModule, PrismaModule],
+  controllers: [MessagesController],
   providers: [MessagesService],
   exports: [MessagesService],
 })
