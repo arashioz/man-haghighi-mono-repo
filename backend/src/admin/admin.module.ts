9b3d29751984a46340467fe4a204ace03d6fb5ec
Arash Balali <a.balali@ittalie.com>
2026-02-27 05:45:47 +0330

    fix any bugs and new feat

--- backend/src/admin/admin.module.ts
+++ backend/src/admin/admin.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 import { CommonModule } from '../common/common.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [CommonModule],
+  imports: [CommonModule, UsersModule],
   controllers: [AdminController],
   providers: [AdminService],
   exports: [AdminService],
